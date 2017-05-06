4c570f0ef388cb00bf13d543d5ccbf427ec3e509
Derek Ahn <git.derek@gmail.com>
2017-05-06 16:54:57 -0700

    Add morgan logger with dev formatter

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import logger from 'morgan';
 import db from './db';
 
 const app = express();
+const port = 3000;
 
+app.use(logger('dev'))
 app.use(express.static('public'))
 const mongoURL = 'mongodb://localhost:27017/links';
 db.connect(mongoURL, (err) => {
