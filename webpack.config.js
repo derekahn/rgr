bdd7cc4098203eb77760639de42c92bf025d09d0
Derek Ahn <git.derek@gmail.com>
2017-05-06 18:25:45 -0700

    Update webpack config to resolve.alias store

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extensions: ['.js', '.jsx'],
     alias: {
       components: path.resolve('client/components'),
+      store: path.resolve('client/store'),
     },
   },
   module: {
